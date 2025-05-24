fb7a9fd365cc799d0bb15e013e500808fd297fdb
shish444 <144669014+shish444@users.noreply.github.com>
2025-05-25 02:12:24 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://military-asset-management-backend.onrender.com/api',
   timeout: 10000,
 });
 
